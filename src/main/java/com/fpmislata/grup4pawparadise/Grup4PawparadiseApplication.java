0f656d1a24847d1a3b4142dfcdbf74e804a19635
MaiaMFM <mariamfm0192@gmail.com>
2023-03-24 11:55:36 +0100

    initial commit

---
+++ src/main/java/com/fpmislata/grup4pawparadise/Grup4PawparadiseApplication.java
@@ -0,0 +1,13 @@
+package com.fpmislata.grup4pawparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grup4PawparadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grup4PawparadiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpmislata/grup4pawparadise/Grup4PawparadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpmislata.grup4pawparadise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grup4PawparadiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
