7d52606900e567ff11bd61a75be6b03b7f271953
MaiaMFM <mariamfm0192@gmail.com>
2023-03-27 19:00:20 +0200

    implements categoryRepository

--- src/main/java/com/fpmislata/grup4pawparadise/persistence/impl/StaticCategoryRepository.java
+++ src/main/java/com/fpmislata/grup4pawparadise/persistence/impl/StaticCategoryRepository.java
@@ -1,7 +1,22 @@
 package com.fpmislata.grup4pawparadise.persistence.impl;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Category;
 import com.fpmislata.grup4pawparadise.persistence.CategoryRepository;
 
 public class StaticCategoryRepository implements CategoryRepository{
+
+    @Override
+    public List<Category> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Category findById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
     
 }
