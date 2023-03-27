5373e2019aacc176ad84e7cd72fb35f05bf3c9fb
MaiaMFM <mariamfm0192@gmail.com>
2023-03-27 19:08:07 +0200

    add implemented methods

--- src/main/java/com/fpmislata/grup4pawparadise/persistence/impl/StaticCategoryRepository.java
+++ src/main/java/com/fpmislata/grup4pawparadise/persistence/impl/StaticCategoryRepository.java
@@ -7,16 +7,26 @@ import com.fpmislata.grup4pawparadise.persistence.CategoryRepository;
 
 public class StaticCategoryRepository implements CategoryRepository{
 
+    List<Category> categories= List.of(
+        new Category(1, "Ropa"),
+        new Category(2, "Correas y collares"),
+        new Category(3, "Viaje"),
+        new Category(4, "Complementos")
+    );
+
     @Override
     public List<Category> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        return this.categories;
     }
 
     @Override
     public Category findById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        for (Category category: this.categories) {
+            if(category.getId() == id){
+                return category;
+            }
+        }
+        return null;
     }
     
 }
