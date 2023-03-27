49b07b68b94bbee07d9f41c5c52d6e854fa7ef85
MaiaMFM <mariamfm0192@gmail.com>
2023-03-27 19:01:22 +0200

    implements categoryRepository

--- src/main/java/com/fpmislata/grup4pawparadise/persistence/CategoryRepository.java
+++ src/main/java/com/fpmislata/grup4pawparadise/persistence/CategoryRepository.java
@@ -1,5 +1,11 @@
 package com.fpmislata.grup4pawparadise.persistence;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Category;
+
 public interface CategoryRepository {
     
+    public List<Category> getAll();
+    public Category findById(int id);
 }
