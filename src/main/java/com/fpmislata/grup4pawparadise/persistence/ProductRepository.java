d0c1688dca78393667920830e594eda30f8d05eb
MaiaMFM <mariamfm0192@gmail.com>
2023-03-27 19:11:54 +0200

    implements productRepository

--- src/main/java/com/fpmislata/grup4pawparadise/persistence/ProductRepository.java
+++ src/main/java/com/fpmislata/grup4pawparadise/persistence/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.fpmislata.grup4pawparadise.persistence;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Product;
+
 public interface ProductRepository {
     
+    public List<Product> getAll();
+    public Product findById(int id);
 }
--- src/main/java/com/fpmislata/grup4pawparadise/persistence/impl/StaticProductRepository.java
+++ src/main/java/com/fpmislata/grup4pawparadise/persistence/impl/StaticProductRepository.java
@@ -1,7 +1,22 @@
 package com.fpmislata.grup4pawparadise.persistence.impl;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Product;
 import com.fpmislata.grup4pawparadise.persistence.ProductRepository;
 
 public class StaticProductRepository implements ProductRepository{
+
+    @Override
+    public List<Product> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Product findById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
     
 }
