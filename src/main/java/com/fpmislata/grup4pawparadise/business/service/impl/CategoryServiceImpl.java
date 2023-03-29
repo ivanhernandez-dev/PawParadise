ad21a8f238b44618974196b6d756162fd9c01863
MaiaMFM <mariamfm0192@gmail.com>
2023-03-29 09:07:01 +0200

    implements categoryService

--- src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,22 @@
 package com.fpmislata.grup4pawparadise.business.service.impl;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Category;
 import com.fpmislata.grup4pawparadise.business.service.CategoryService;
 
 public class CategoryServiceImpl implements CategoryService{
+
+    @Override
+    public List<Category> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Category findById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
     
 }
