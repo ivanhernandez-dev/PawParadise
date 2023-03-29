78100eb025263a3ee7c4c27b1425cbeb4252f271
MaiaMFM <mariamfm0192@gmail.com>
2023-03-29 09:08:43 +0200

    implements productService

--- src/main/java/com/fpmislata/grup4pawparadise/business/service/ProductService.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/ProductService.java
@@ -1,5 +1,12 @@
 package com.fpmislata.grup4pawparadise.business.service;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Product;
+
 public interface ProductService {
     
+    public List<Product> getAll();
+    public Product findById(int id);
+    public Product findByCategoryId(int categoryId);
 }
--- src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/ProductServiceImpl.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/ProductServiceImpl.java
@@ -1,8 +1,29 @@
 package com.fpmislata.grup4pawparadise.business.service.impl;
 
+import java.util.List;
+
+import com.fpmislata.grup4pawparadise.business.entity.Product;
 import com.fpmislata.grup4pawparadise.business.service.ProductService;
 
 public class ProductServiceImpl implements ProductService{
+
+    @Override
+    public List<Product> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Product findById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Product findByCategoryId(int categoryId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByCategoryId'");
+    }
     
     
 }
