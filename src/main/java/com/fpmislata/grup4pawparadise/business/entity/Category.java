5b7801bf2a54e14c8d15913f248d8ac2f21d3651
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-04-19 09:59:00 +0200

    Create constructors

--- src/main/java/com/fpmislata/grup4pawparadise/business/entity/Category.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/entity/Category.java
@@ -1,5 +1,6 @@
 package com.fpmislata.grup4pawparadise.business.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
@@ -12,6 +13,20 @@ public class Category {
         this.id = id;
         this.name = name;
         this.products = products;
+    }
+
+    public Category(int id, String name, List<Category> categories, List<Product> products) {
+        this.id = id;
+        this.name = name;
+        this.categories = categories;
+        this.products = products;
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.categories = new ArrayList<>();
+        this.products = new ArrayList<>();
     }
 
     public int getId() {
