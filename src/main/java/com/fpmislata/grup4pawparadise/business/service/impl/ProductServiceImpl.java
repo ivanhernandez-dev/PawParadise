9c443bc278a4727c605c7dde4830fccd3d8ec6b7
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-04-28 11:43:22 +0200

    Update ProductServiceImpl

--- src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/ProductServiceImpl.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/ProductServiceImpl.java
@@ -1,29 +1,42 @@
 package com.fpmislata.grup4pawparadise.business.service.impl;
 
-import java.util.List;
-
+import com.fpmislata.grup4pawparadise.business.entity.Category;
 import com.fpmislata.grup4pawparadise.business.entity.Product;
 import com.fpmislata.grup4pawparadise.business.service.ProductService;
+import com.fpmislata.grup4pawparadise.exception.ResourceNotFoundException;
+import com.fpmislata.grup4pawparadise.persistence.CategoryRepository;
 import com.fpmislata.grup4pawparadise.persistence.ProductRepository;
+import com.fpmislata.grup4pawparadise.persistence.impl.JDBCCategoryRepository;
+import com.fpmislata.grup4pawparadise.persistence.impl.JDBCProductRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductServiceImpl implements ProductService{
 
-    private ProductRepository productRepository = new StaticProductRepository();
+    private ProductRepository productRepository = new JDBCProductRepository();
+    private CategoryRepository categoryRepository = new JDBCCategoryRepository();
 
     @Override
-    public List<Product> getAll() {
-        return this.productRepository.getAll();
+    public List<Product> getAll(String language) {
+        return this.productRepository.getAll(language);
     }
 
     @Override
-    public Product findById(int id) throws Exception {
-        return this.productRepository.findById(id);
+    public Product findById(int id, String language) throws ResourceNotFoundException {
+        return this.productRepository.findById(id, language);
     }
 
     @Override
-    public List<Product> findByCategoryId(int categoryId) {
-        return this.productRepository.findByCategoryId(categoryId);
+    public List<Product> findByCategoryIdWithSuccessors(int categoryId, String language) {
+        List<Category> categories = this.categoryRepository.getSuccessorsByParentId(categoryId, language);
+
+        List<Integer> categoryIds = new ArrayList<>();
+        categoryIds.add(categoryId);
+        for (Category category : categories) {
+            categoryIds.add(category.getId());
+        }
+
+        return this.productRepository.findByCategoryIds(categoryIds, language);
     }
-    
-    
 }
