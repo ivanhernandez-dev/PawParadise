0e26273b47dc83e67fa60861a36c6d59c3e30d72
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-04-28 11:45:25 +0200

    Update product controller

--- src/main/java/com/fpmislata/grup4pawparadise/controller/ProductController.java
+++ src/main/java/com/fpmislata/grup4pawparadise/controller/ProductController.java
@@ -1,37 +1,25 @@
 package com.fpmislata.grup4pawparadise.controller;
 
 
+import com.fpmislata.grup4pawparadise.business.service.ProductService;
+import com.fpmislata.grup4pawparadise.business.service.impl.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.fpmislata.grup4pawparadise.business.service.ProductService;
-import com.fpmislata.grup4pawparadise.business.service.impl.ProductServiceImpl;
-
 @Controller
 public class ProductController {
     
     private ProductService productService = new ProductServiceImpl();
-
-    @GetMapping("/{categoryId}")
-    public String findByCategoryId(@PathVariable("categoryId") int categoryId, Model model){
-        model.addAttribute("products", this.productService.findByCategoryId(categoryId));
-        return "products";
-    }
-
-    @GetMapping("/productos/{id}")
-    public String findById(@PathVariable("id") int id, Model model){
-
+    @GetMapping("/{language}/productos/{productId}")
+    public String findById(@PathVariable("productId") int productId, Model model, @PathVariable String language){
         try {
-            model.addAttribute("products", this.productService.findById(id));
+            model.addAttribute("products", this.productService.findById(productId, language));
+            model.addAttribute("language", language);
             return "product";
         } catch (Exception e) {
             return "error";
         }
-        
-        
     }
-
-
 }
