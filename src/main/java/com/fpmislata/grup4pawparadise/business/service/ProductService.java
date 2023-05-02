bb2d1d4d8a027ac8059ebad792467c685e747825
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-05-02 09:12:48 +0200

    Delete method getAll from ProductService

--- src/main/java/com/fpmislata/grup4pawparadise/business/service/ProductService.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/ProductService.java
@@ -6,8 +6,7 @@ import com.fpmislata.grup4pawparadise.exception.ResourceNotFoundException;
 import java.util.List;
 
 public interface ProductService {
-    
-    public List<Product> getAll(String language);
+
     public Product findById(int id, String language) throws ResourceNotFoundException;
     public List<Product> findByCategoryIdWithSuccessors(int categoryId, String language) throws ResourceNotFoundException;
 }
