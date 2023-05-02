5937516acde72511bb7e6e889a1a559217864e4f
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-05-02 08:35:07 +0200

    Delete unused constructors from Product

--- src/main/java/com/fpmislata/grup4pawparadise/business/entity/Product.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/entity/Product.java
@@ -14,26 +14,6 @@ public class Product {
 
     private final static String DEFAULT_IMAGE_URL = "https://example.com/default_image.jpg";
 
-    public Product(int id, String name, String price, int stock, String description) {
-        this(id,name);
-        this.price = new BigDecimal(price);
-        this.stock = stock;
-        this.description = description;
-    }
-
-    public Product(int id, String name, String price, String description, String descriptionHTML, String image) {
-        this(id,name);
-        this.price = new BigDecimal(price);
-        this.description = description;
-        this.descriptionHTML = descriptionHTML;
-        this.image = image;
-    }
-
-    public Product(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     public Product(int id, String name, String description, String descriptionHTML, String price, int stock, String image) {
         this.id = id;
         this.name = name;
