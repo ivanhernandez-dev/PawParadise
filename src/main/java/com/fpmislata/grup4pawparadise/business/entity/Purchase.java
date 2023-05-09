d97a4b6014241409dc37cfaeb22ce15be922294f
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-05-09 10:39:30 +0200

    Change id variable

--- src/main/java/com/fpmislata/grup4pawparadise/business/entity/Purchase.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/entity/Purchase.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 public class Purchase {
+    private final int id;
     private Date date;
     private int status;
     private int idUser;
     private List<ShoppingCart> shoppingCartList;
 
-    public Purchase(Date date, int status, int idUser, List<ShoppingCart> shoppingCartList) {
+    public Purchase(int id, Date date, int status, int idUser, List<ShoppingCart> shoppingCartList) {
+        this.id = id;
         this.date = date;
         this.status = status;
         this.idUser = idUser;
--- src/main/java/com/fpmislata/grup4pawparadise/business/entity/ShoppingCart.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/entity/ShoppingCart.java
@@ -1,12 +1,10 @@
 package com.fpmislata.grup4pawparadise.business.entity;
 
 public class ShoppingCart {
-    private final int id;
     private int quantity;
     private Product product;
 
     public ShoppingCart(int id, int quantity, Product product) {
-        this.id = id;
         this.quantity = quantity;
         this.product = product;
     }
