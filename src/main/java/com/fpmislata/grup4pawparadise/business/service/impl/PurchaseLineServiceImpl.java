f8edfb26cb9b98fd5b7f9a8c05cafb4e4eb91a86
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-05-26 09:26:03 +0200

    Add PurchaseLineService

---
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/impl/PurchaseLineServiceImpl.java
@@ -0,0 +1,37 @@
+package com.fpmislata.grup4pawparadise.business.service.impl;
+
+import com.fpmislata.grup4pawparadise.business.service.PurchaseLineService;
+import com.fpmislata.grup4pawparadise.persistence.PurchaseLineRepository;
+import com.fpmislata.grup4pawparadise.persistence.impl.JDBCPurchaseLineRepository;
+
+public class PurchaseLineServiceImpl implements PurchaseLineService {
+
+    private PurchaseLineRepository purchaseLineRepository =  new JDBCPurchaseLineRepository();
+
+    @Override
+    public void insert(int idPurchase, int idProduct, int quantity) {
+        checkQuantity(quantity);
+        purchaseLineRepository.insert(idPurchase, idProduct, quantity);
+    }
+
+    @Override
+    public void update(int idPurchase, int idProduct, int quantity) {
+        checkQuantity(quantity);
+        purchaseLineRepository.update(idPurchase, idProduct, quantity);
+    }
+
+    @Override
+    public void delete(int idPurchase, int productId) {
+        purchaseLineRepository.delete(idPurchase, productId);
+    }
+
+    private void checkQuantity(int quantity) {
+        final int MAX_QUANTITY = 15;
+        final int MIN_QUANTITY = 1;
+
+        if (quantity < MIN_QUANTITY || quantity > MAX_QUANTITY) {
+            throw new IllegalArgumentException("The quantity must be between " + MIN_QUANTITY + " and " + MAX_QUANTITY +
+                    ".");
+        }
+    }
+}
