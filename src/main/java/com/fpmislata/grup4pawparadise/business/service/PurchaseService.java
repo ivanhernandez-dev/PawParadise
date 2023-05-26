af3d852e9ed457044ae991dbd667a24bb41816c4
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-05-26 09:23:14 +0200

    Add PurchaseService

--- src/main/java/com/fpmislata/grup4pawparadise/business/service/PurchaseService.java
+++ src/main/java/com/fpmislata/grup4pawparadise/business/service/PurchaseService.java
@@ -1,9 +1,10 @@
 package com.fpmislata.grup4pawparadise.business.service;
 
 import com.fpmislata.grup4pawparadise.business.entity.Purchase;
+import com.fpmislata.grup4pawparadise.exception.ResourceNotFoundException;
 
 public interface PurchaseService {
-    Purchase getByUserIdWhereStatusActive(int userId);
-    void insert(Purchase purchase);
-    void update(Purchase purchase);
+
+    Purchase getByUserIdWhereStatusActive(int idCustomer, String language) throws ResourceNotFoundException;
+    void updatePurchaseStatus(int idPurchase, int status, int idCustomer);
 }
