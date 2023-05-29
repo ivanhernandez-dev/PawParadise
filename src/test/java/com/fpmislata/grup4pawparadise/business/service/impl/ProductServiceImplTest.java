72201d4271efcbe557001305fd6c6d56ce6f7aff
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-05-29 18:49:54 +0200

    Add tests for getByName

--- src/test/java/com/fpmislata/grup4pawparadise/business/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/fpmislata/grup4pawparadise/business/service/impl/ProductServiceImplTest.java
@@ -50,7 +50,13 @@ public class ProductServiceImplTest {
 
         Product actualProduct = productService.getById(id, language);
 
-        assertEquals(expectedProduct, actualProduct, "The products should be equal");
+        assertAll(
+                () -> assertEquals(expectedProduct, actualProduct,
+                        "The product returned should be equal to the expected product"),
+                () -> verify(productRepository, description("The method getById should be called with parameters id=" +
+                        id + " and language=" + language))
+                        .getById(id, language)
+        );
     }
 
     @DisplayName("Test getById(int, String) with non-existing product")
@@ -61,7 +67,14 @@ public class ProductServiceImplTest {
 
         when(productRepository.getById(-1, language)).thenThrow(ResourceNotFoundException.class);
 
-        assertThrows(ResourceNotFoundException.class, () -> productService.getById(id, language));
+        assertAll(
+                () -> assertThrows(ResourceNotFoundException.class, () -> productService.getById(id, language),
+                        "The method should throw a ResourceNotFoundException"),
+                () -> verify(productRepository, description("The method getById should be called with parameters id=" +
+                        id + " and language=" + language))
+                        .getById(id, language)
+
+        );
     }
 
     @DisplayName("Test getByCategoryIdWithSuccessors(int)")
@@ -89,12 +102,83 @@ public class ProductServiceImplTest {
 
         assertAll(
                 () -> verify(productRepository,
-                        description("The method getByCategoryIds should be called with parameters 1, 2, 3 and language es"))
+                        description("The method getByCategoryIds should be called with parameters=" + List.of(1, 2, 3) +
+                                " and language=" + language))
                         .getByCategoryIds(List.of(1, 2, 3), language),
                 () -> verify(categoryRepository,
                         description("The method getSuccessorsByParentId should be called with parameter 1 and language es"))
                         .getSuccessorsByParentId(categoryId, language),
                 () -> assertEquals(expectedProducts, actualProducts, "The lists should be equal")
+        );
+    }
+
+    @DisplayName("Test getByName(String, String) with null name")
+    @Test
+    void getByNameTestShouldThrowIllegalArgumentException() {
+        String name = null;
+        String language = "es";
+
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> productService.getByName(name, language),
+                        "The method should throw an IllegalArgumentException"),
+                () -> verify(productRepository, never()).getByName(name, language)
+        );
+    }
+
+    @DisplayName("Test getByName(String, String) with empty name")
+    @Test
+    void getByNameTestShouldThrowIllegalArgumentException2() {
+        String name = "";
+        String language = "es";
+
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> productService.getByName(name, language),
+                        "The method should throw an IllegalArgumentException"),
+                () -> verify(productRepository, never()).getByName(name, language)
+        );
+    }
+
+    @DisplayName("Test getByName(String, String) with an existing name")
+    @Test
+    void getByNameTestShouldReturnProduct() throws ResourceNotFoundException {
+        List<Product> expectedProducts = List.of(
+                new Product(1, "Car 1", "Description 1", productFeatures,
+                        "10.00", 30, "https://example.com/image.jpg"),
+                new Product(2, "Car 2", "Description 2", productFeatures,
+                        "20.00", 20, "https://example.com/image.jpg")
+        );
+        String name = "Car";
+        String language = "es";
+
+        when(productRepository.getByName(name, language)).thenReturn(expectedProducts);
+
+        List<Product> actualProducts = productService.getByName(name, language);
+
+        assertAll(
+                () -> verify(productRepository,
+                        description("The method getByName should be called with parameters name=" + name +
+                                " and language=" + language))
+                        .getByName(name, language),
+                () -> assertEquals(expectedProducts, actualProducts, "The lists should be equal")
+        );
+    }
+
+    @DisplayName("Test getByName(String, String) with a non-existing name")
+    @Test
+    void getByNameTestShouldThrowResourceNotFoundException() {
+        String name = "Car";
+        String language = "es";
+        List<Product> expectedProducts = List.of();
+
+        when(productRepository.getByName(name, language)).thenReturn(expectedProducts);
+
+        assertAll(
+                () -> assertThrows(ResourceNotFoundException.class, () -> productService.getByName(name, language),
+                        "The method should throw a ResourceNotFoundException"),
+                () -> verify(productRepository,
+                        description("The method getByName should be called with parameters name=" + name +
+                                " and language=" + language))
+                        .getByName(name, language)
         );
     }
 }
