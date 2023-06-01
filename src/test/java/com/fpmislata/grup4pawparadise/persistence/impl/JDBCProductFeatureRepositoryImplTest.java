a24cb0e807c2ecf785dc7eeebbefbee4d7e9b776
Iván Hernández Ojeda <ihernandezcipfpmislata@gmail.com>
2023-06-01 20:35:02 +0200

    Add test getByProductIdAndLanguage

--- src/test/java/com/fpmislata/grup4pawparadise/persistence/impl/JDBCProductFeatureRepositoryImplTest.java
+++ src/test/java/com/fpmislata/grup4pawparadise/persistence/impl/JDBCProductFeatureRepositoryImplTest.java
@@ -2,8 +2,10 @@ package com.fpmislata.grup4pawparadise.persistence.impl;
 
 import com.fpmislata.grup4pawparadise.business.entity.ProductFeature;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -18,14 +20,30 @@ class JDBCProductFeatureRepositoryImplTest {
     public static void setup() {
         productFeatureRepository = new JDBCProductFeatureRepositoryImpl();
         DataSource dataSource = new EmbeddedDatabaseBuilder()
-                .addScript("")
-                .addScript("")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("db/sql/product_feature_tables.sql")
+                .addScript("db/sql/product_feature_data.sql")
                 .build();
         productFeatureRepository.setDatasource(dataSource);
     }
 
+    @DisplayName("Test getByProductIdAndLanguage(int, String)")
     @Test
     void getByProductIdAndLanguage() {
-        List<ProductFeature> productFeatures = productFeatureRepository.getByProductIdAndLanguage(1, "ES");
+        List<ProductFeature> expectedProductFeatures = List.of(
+                new ProductFeature(1, "Proporciona calidez y comodidad durante todo el día"),
+                new ProductFeature(2, "Ideal para climas frescos"),
+                new ProductFeature(3, "Fabricada con materiales de alta calidad"),
+                new ProductFeature(4, "Diseño moderno y versátil"),
+                new ProductFeature(5, "Capucha ajustable y bolsillos funcionales"),
+                new ProductFeature(6, "Disponible en varios colores"),
+                new ProductFeature(7, "Talla: unisex"),
+                new ProductFeature(8, "Tejido transpirable que proporciona una buena ventilación")
+        );
+
+        List<ProductFeature> productFeatures = productFeatureRepository.getByProductIdAndLanguage(1, "es");
+
+        assertEquals(expectedProductFeatures, productFeatures,
+                "The product features retrieved should be the equal to the expected ones");
     }
 }
