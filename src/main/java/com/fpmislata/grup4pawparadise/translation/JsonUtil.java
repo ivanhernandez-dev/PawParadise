801613833afd7ae471b9e14bf58f53ca3051ae2c
MariaMFM <mariamfm0192@gmail.com>
2023-06-05 09:12:51 +0200

    Add readJsonData method to JsonUtil class

--- src/main/java/com/fpmislata/grup4pawparadise/translation/JsonUtil.java
+++ src/main/java/com/fpmislata/grup4pawparadise/translation/JsonUtil.java
@@ -1,5 +1,31 @@
 package com.fpmislata.grup4pawparadise.translation;
 
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.springframework.core.io.ClassPathResource;
+
 public class JsonUtil {
-    
+    public JSONObject readJsonData(String language) {
+
+        JSONObject jsonData = null;
+        String filePath = null;
+
+        if (language.equals("es")) {
+            filePath = "static/json/translate_es.json";
+        } else if (language.equals("en")) {
+            filePath = "static/json/translate_en.json";
+        }
+
+        try {
+            ClassPathResource staticDataResource = new ClassPathResource(filePath);
+            String staticDataString = IOUtils.toString(staticDataResource.getInputStream(), StandardCharsets.UTF_8);
+            jsonData = new JSONObject(staticDataString);
+
+        } catch (Exception e) {
+
+        }
+        return jsonData;
+    }
 }
