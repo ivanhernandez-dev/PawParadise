ba7009553a23181a0828dcce5301f76989d81b99
MariaMFM <mariamfm0192@gmail.com>
2023-06-05 09:36:14 +0200

    Resolving conflicts in ProductController

--- src/main/java/com/fpmislata/grup4pawparadise/controller/ProductController.java
+++ src/main/java/com/fpmislata/grup4pawparadise/controller/ProductController.java
@@ -5,7 +5,11 @@ import com.fpmislata.grup4pawparadise.business.service.CategoryService;
 import com.fpmislata.grup4pawparadise.business.service.ProductService;
 import com.fpmislata.grup4pawparadise.business.service.impl.CategoryServiceImpl;
 import com.fpmislata.grup4pawparadise.business.service.impl.ProductServiceImpl;
+import com.fpmislata.grup4pawparadise.translation.JsonUtil;
+
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +18,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class ProductController {
     
+    private JsonUtil jsonUtil = new JsonUtil();
     private ProductService productService = new ProductServiceImpl();
     private CategoryService categoryService = new CategoryServiceImpl();
 
     @GetMapping("/{language}/productos/{productId}")
     public String getById(@PathVariable("productId") int productId, Model model, @PathVariable String language){
         try {
+            JSONObject jsonData = jsonUtil.readJsonData(language);
+
+            model.addAttribute("jsonData", jsonData);
             model.addAttribute("allCategories", this.categoryService.getAll(language));
             model.addAttribute("product", this.productService.getById(productId, language));
             model.addAttribute("language", language);
@@ -34,7 +42,10 @@ public class ProductController {
     @GetMapping("/{language}/productos/buscar")
     public String getByName(Model model, @PathVariable String language, HttpServletRequest httpServletRequest){
         try {
+            JSONObject jsonData = jsonUtil.readJsonData(language);
             String name = httpServletRequest.getParameter("nombre");
+            
+            model.addAttribute("jsonData", jsonData);
             model.addAttribute("allCategories", this.categoryService.getAll(language));
             model.addAttribute("products", this.productService.getByName(name, language));
             model.addAttribute("actualCategoryName", name);
