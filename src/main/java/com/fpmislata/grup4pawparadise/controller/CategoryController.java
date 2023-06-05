7e736a31236771afc1f2b83b551e46dc08f85f00
MariaMFM <mariamfm0192@gmail.com>
2023-06-05 09:21:22 +0200

    Use readJsonData method in CategoryController

--- src/main/java/com/fpmislata/grup4pawparadise/controller/CategoryController.java
+++ src/main/java/com/fpmislata/grup4pawparadise/controller/CategoryController.java
@@ -4,6 +4,9 @@ import com.fpmislata.grup4pawparadise.business.service.CategoryService;
 import com.fpmislata.grup4pawparadise.business.service.ProductService;
 import com.fpmislata.grup4pawparadise.business.service.impl.CategoryServiceImpl;
 import com.fpmislata.grup4pawparadise.business.service.impl.ProductServiceImpl;
+import com.fpmislata.grup4pawparadise.translation.JsonUtil;
+
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +15,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CategoryController {
-
+    
+    private JsonUtil jsonUtil = new JsonUtil();
     private CategoryService categoryService = new CategoryServiceImpl();
     private ProductService productService = new ProductServiceImpl();
 
     @GetMapping("/{language}/categorias/{categoryId}")
     public String getByCategoryIdWithSuccessors(@PathVariable("categoryId") int categoryId, Model model, @PathVariable String language){
         try {
+            JSONObject jsonData = jsonUtil.readJsonData(language);
+
+            model.addAttribute("jsonData", jsonData);
             model.addAttribute("allCategories", this.categoryService.getAll(language));
             model.addAttribute("categories", this.categoryService.getChildrenByParentId(categoryId, language));
             model.addAttribute("products", this.productService.getByCategoryIdWithSuccessors(categoryId, language));
